194d6a119a40b7f2582cfa915eac324cd7e739ff
NYZCoder <94626748+NYZCoder@users.noreply.github.com>
2022-05-12 20:18:22 +0200

    removed buttons

--- happy_mucher_frontend/lib/homepage.dart
+++ happy_mucher_frontend/lib/homepage.dart
@@ -59,45 +59,7 @@ class _MyHomePageState extends State<MyHomePage> {
             SizedBox(
               height: 16,
             ),
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: RaisedButton(
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 30),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => inventory()));
-                },
-                color: Colors.green,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30))),
-                child: Text(
-                  "View Inventory",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: RaisedButton(
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 30),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => budget()));
-                },
-                color: Colors.green,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30))),
-                child: Text(
-                  "Create Your Budget",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
+
           ],
         ),
       ),
