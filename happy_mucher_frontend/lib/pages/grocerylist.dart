1c971dafc61ddc270415f27b3ff99a24bde40c42
Ponalo Notwane <94800625+Ponalo-ss@users.noreply.github.com>
2022-05-12 23:22:35 +0200

    Update grocerylist.dart

--- happy_mucher_frontend/lib/pages/grocerylist.dart
+++ happy_mucher_frontend/lib/pages/grocerylist.dart
@@ -1,77 +1,102 @@
-import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter/material.dart';
-import 'package:happy_mucher_frontend/pages/grocerylist.dart';
+import 'package:happy_mucher_frontend/dialogs/add_grocery.dialog.dart';
 
-void main() {
-  group(
-    'Added testing for grocery list',
-    () {
-      const testApp = MaterialApp(
-        home: Scaffold(
-          body: GroceryListPage(),
-        ),
-      );
-      testWidgets(
-        'Testing if page is empty on start up',
-        (WidgetTester tester) async {
-          //test to see if the list is empty on initial start up
-          //runs the app and checks the list to see if it has 0 list tile widgets
-          //success if finds 0 widgets
-          await tester.pumpWidget(testApp);
+class GroceryListPage extends StatefulWidget {
+  const GroceryListPage({Key? key}) : super(key: key);
 
-          final inventoryList = find.byKey(const Key('Grocery_ListView'));
-          expect(inventoryList, findsOneWidget);
-
-          final listTiles = find.byType(ListTile);
-          expect(listTiles, findsNothing);
-        },
-      );
-
-      testWidgets(
-        'Testing if an item is added after entering on dialog box',
-        (WidgetTester tester) async {
-          //test to see if the adding functionality works
-          //adds the object ['apples', '10']
-          await tester.pumpWidget(testApp);
-
-          //finds the add button so that it can be pressed to open dialog
-          final dialogEnterButton =
-              find.byKey(const Key('addToGroceryListButton'));
-          expect(dialogEnterButton, findsOneWidget);
-
-          await tester.tap(dialogEnterButton);
-          await tester.pumpAndSettle(const Duration(milliseconds: 300));
-
-          //finds all buttons on the dialog page
-          //then adds text to the text fields and selects current date
-          //then clicks OK
-          //then checks if the list contains a list tile
-          final dialogReturnName =
-              find.byKey(const Key('groceryListDialogNameField'));
-          expect(dialogReturnName, findsOneWidget);
-
-          final dialogReturnQuantity =
-              find.byKey(const Key('groceryListDialogPriceField'));
-          expect(dialogReturnQuantity, findsOneWidget);
-
-          await tester.enterText(dialogReturnName, 'Apples');
-          await tester.enterText(dialogReturnQuantity, '10');
-
-          await tester.pumpAndSettle(const Duration(milliseconds: 300));
-
-          final dialogReturnButton =
-              find.byKey(const Key('groceryListDialogAddButton'));
-          expect(dialogReturnButton, findsOneWidget);
-
-          await tester.tap(dialogReturnButton);
-          await tester.pumpAndSettle(const Duration(milliseconds: 300));
-
-          final inventoryList = find.byKey(const Key('Grocery_ListView'));
-          expect(inventoryList, findsOneWidget);
-          final listTiles = find.byType(CheckboxListTile);
-          expect(listTiles, findsOneWidget);
-        },
-      );
-    },
-  );
+  @override
+  State<GroceryListPage> createState() => _GroceryListPageState();
 }
+
+class _GroceryListPageState extends State<GroceryListPage> {
+  var checkedValue = false;
+
+  List<GroceryListItem> inventoryList = [];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: const Icon(Icons.fastfood),
+        title: const Text(
+          'Grocery List',
+        ),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              key: const Key('Grocery_ListView'),
+              itemCount: inventoryList.length,
+              itemBuilder: (context, index) {
+                final item = inventoryList[index];
+
+                return CheckboxListTile(
+                  controlAffinity: ListTileControlAffinity.leading,
+                  value: item.value,
+                  onChanged: (checkedValue) =>
+                      setState(() => item.value = checkedValue!),
+                  title: Text(item.itemName),
+                  secondary: Text('R${item.Price}'),
+                );
+              },
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24.0),
+                child: IconButton(
+                  key: const Key('addToGroceryListButton'),
+                  onPressed: () async {
+                    final returnedItem = await addGroceryDialog(context);
+                    if (returnedItem != null) {
+                      setState(() {
+                        inventoryList.add(GroceryListItem(
+                          itemName: returnedItem.name,
+                          Price: returnedItem.price,
+                          value: false,
+                        ));
+                      });
+                    }
+                  },
+                  icon: const Icon(Icons.add_circle),
+                  iconSize: 64.0,
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class GroceryListItem {
+  final String itemName;
+  final int Price;
+  bool value;
+  GroceryListItem({
+    required this.Price,
+    required this.itemName,
+    required this.value,
+  });
+}
+
+//STRUCTURE
+//SCAFFOLD
+//  APPBAR
+//    ICON + TEXT
+//  COLUMN
+//    EXPANDED (TO FILL UP SCREEN)
+//      LISTVIEW (LIST OF TILES)
+//       LIST TILE (LIST THAT CONTAINS INFO)
+//         TEXT + TEXT
+//    ROW (BOTTOM 2 BUTTONS)
+//      PADDING
+//        ICONBUTTON
+//      PADDING
+//        ICONBUTTON
+
+//CLASS OF THE RETURNED LIST ITEM
