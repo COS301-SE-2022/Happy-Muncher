aed9ab078d0761cc94e93de31218f30d79e9b458
NYZCoder <u19001836@tuks.co.za>
2022-05-31 22:32:49 +0200

    error resolved

--- happy_mucher_frontend/lib/pages/weekday.dart
+++ happy_mucher_frontend/lib/pages/weekday.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:url_launcher/url_launcher.dart';
 
 class Month extends StatefulWidget {
   const Month({Key? key, required this.month}) : super(key: key);
