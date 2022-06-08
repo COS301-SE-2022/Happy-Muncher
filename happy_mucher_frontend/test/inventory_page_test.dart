665c9d4b634120aeae142ceebac6366a0d175f64
Dylan-Ku <ku.dylan@gmail.com>
2022-06-08 15:34:35 +0200

    test for adding item

--- happy_mucher_frontend/test/inventory_page_test.dart
+++ happy_mucher_frontend/test/inventory_page_test.dart
@@ -67,6 +67,60 @@ void main() {
           expect(listviews, findsNWidgets(3));
         },
       );
+
+      testWidgets(
+        'Testing if an item is added after entering on dialog box',
+        (WidgetTester tester) async {
+          //test to see if the adding functionality works
+          //adds the object ['apples', '10', Current date]
+          await tester.pumpWidget(testApp);
+
+          //finds the add button so that it can be pressed to open dialog
+          final dialogEnterButton =
+              find.byKey(const Key('addToInventoryButton'));
+          expect(dialogEnterButton, findsOneWidget);
+
+          await tester.tap(dialogEnterButton);
+          await tester.pumpAndSettle(const Duration(milliseconds: 300));
+
+          //finds all buttons on the dialog page
+          //then adds text to the text fields and selects current date
+          //then clicks OK
+          //then checks if the list contains a list tile
+          final dialogReturnName =
+              find.byKey(const Key('inventoryDialogNameField'));
+          expect(dialogReturnName, findsOneWidget);
+
+          final dialogReturnQuantity =
+              find.byKey(const Key('inventoryDialogQuantityField'));
+          expect(dialogReturnQuantity, findsOneWidget);
+
+          final dialogReturnDate =
+              find.byKey(const Key('inventoryDialogCalendarPickButton'));
+          expect(dialogReturnDate, findsOneWidget);
+
+          await tester.enterText(dialogReturnName, 'Apples');
+          await tester.enterText(dialogReturnQuantity, '10');
+
+          await tester.tap(dialogReturnDate);
+          await tester.pumpAndSettle(const Duration(milliseconds: 300));
+          await tester.tap(find.text('OK'));
+
+          await tester.pumpAndSettle(const Duration(milliseconds: 300));
+
+          final dialogReturnButton =
+              find.byKey(const Key('inventoryDialogAddButton'));
+          expect(dialogReturnButton, findsOneWidget);
+
+          await tester.tap(dialogReturnButton);
+          await tester.pumpAndSettle(const Duration(milliseconds: 300));
+
+          final inventoryList = find.byKey(const Key('Inventory_ListView'));
+          expect(inventoryList, findsOneWidget);
+          final listTiles = find.byType(ListTile);
+          expect(listTiles, findsOneWidget);
+        },
+      );
     },
   );
 }
