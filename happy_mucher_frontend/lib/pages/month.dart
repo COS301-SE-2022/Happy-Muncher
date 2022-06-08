24471d73084410595a19ec841a6df1097a853e61
juwiCarrim <u20435780@tuks.co.za>
2022-06-08 17:21:00 +0200

    Merge branch 'develop' of https://github.com/COS301-SE-2022/Happy-Muncher into feature/budget

--- happy_mucher_frontend/lib/pages/month.dart
+++ happy_mucher_frontend/lib/pages/month.dart
@@ -1,13 +1,29 @@
 import 'package:flutter/material.dart';
+import 'grocerylist.dart';
+import 'package:happy_mucher_frontend/pages/grocerylist.dart';
 
 class Month extends StatefulWidget {
-  const Month({Key? key, required this.month}) : super(key: key);
+  const Month({Key? key, this.month = "", this.price = 0, this.glSpent = 0})
+      : super(key: key);
   final String month;
+  final int price; //total Gl price estimate
+  final int glSpent; //GL amount spent after shopping
   @override
   State<Month> createState() => MyMonthState();
 }
 
 class MyMonthState extends State<Month> {
+  // int groceryListTotal() {
+  //   List<GroceryListItem> gl = GroceryListPage.inventoryList;
+
+  //   int total = 0;
+  //   gl.forEach((element) {
+  //     total = total + element.Price;
+  //   });
+  //   return total;
+  // }
+
+  //int tot = groceryListTotal();
   //static const IconData money = IconData(0xe3f8, fontFamily: 'MaterialIcons');
   final budgetController = TextEditingController();
 
@@ -16,29 +32,39 @@ class MyMonthState extends State<Month> {
   String budget = "";
   double totRem = 0;
   double totSpent = 0;
+  double totBudget = 0;
+
+  String compMessage = "";
 
   //variables for week 1
   final spentOneController = TextEditingController();
   String spent1 = "0.0";
   String rem1 = "0";
+  bool editOne = false;
 
   //variables for week 2
   final spentTwoController = TextEditingController();
   String spent2 = "0.0";
   String rem2 = "0";
+  bool editTwo = false;
 
   //variables for week 3
   final spentThreeController = TextEditingController();
   String spent3 = "0.0";
   String rem3 = "0";
+  bool editThree = false;
 
   //variables for week 4
   final spentFourController = TextEditingController();
   String spent4 = "0.0";
   String rem4 = "0";
+  bool editFour = false;
 
   @override
   Widget build(BuildContext context) {
+    // setState(() {
+    //   totSpent = widget.glSpent.toDouble();
+    // });
     return Scaffold(
       appBar: AppBar(
         // Here we take the value from the MyHomePage object that was created by
@@ -56,12 +82,22 @@ class MyMonthState extends State<Month> {
             onPressed: () {
               setState(() {
                 input = budgetController.text;
-                bud = double.parse(input);
+                if (input.length > 0) {
+                  bud = double.parse(input);
+                } else
+                  () {
+                    bud = 0;
+                  };
+
+                totBudget = bud;
                 totRem = bud;
+
                 bud = bud / 4;
                 budget = bud.toString();
 
-                totSpent = 0;
+                //totSpent = 0;
+                totSpent += widget.glSpent.toDouble();
+                totRem -= totSpent;
                 rem1 = budget;
                 rem2 = budget;
                 rem3 = budget;
@@ -81,7 +117,10 @@ class MyMonthState extends State<Month> {
           const SizedBox(height: 24),
           WeekFour(),
           const SizedBox(height: 32),
-          Totals()
+          Totals(),
+          EstTotal(),
+          //Comparison(),
+          //showAlertDialog(context)
         ],
       ),
     );
@@ -132,31 +171,45 @@ class MyMonthState extends State<Month> {
         Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
           const Text("Amount Spent"),
           Flexible(
-              child: TextField(
-            key: Key("spent1"),
-            textAlign: TextAlign.right,
-            controller: spentOneController,
-            decoration: const InputDecoration(
-              hintText: 'R 0',
-            ),
-            keyboardType: TextInputType.number,
-            textInputAction: TextInputAction.done,
-            onSubmitted: (value) {
-              setState(() {
-                spent1 = spentOneController.text;
-                totRem -= double.parse(spent1);
-                double left = double.parse(spent1);
-                totSpent += double.parse(spent1);
-                left = bud - left;
-                rem1 = left.toString();
-              });
-            },
-          ))
+              child: !editOne
+                  ? Text('R' + spent1)
+                  : TextField(
+                      key: Key("spent1"),
+                      textAlign: TextAlign.right,
+                      controller: spentOneController,
+                      decoration: const InputDecoration(
+                        hintText: 'R 0',
+                      ),
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.done,
+                      onSubmitted: (value) {
+                        setState(() {
+                          spent1 = spentOneController.text;
+                          totRem -= double.parse(spent1);
+                          double left = double.parse(spent1);
+                          totSpent += double.parse(spent1);
+                          left = bud - left;
+                          rem1 = left.toString();
+                          editOne = false;
+                        });
+                      },
+                    )),
         ]),
         const SizedBox(height: 10),
         Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [const Text("Amount Remaining"), Text("R " + rem1)]),
+        IconButton(
+          alignment: Alignment.bottomRight,
+          //color: Colors.green,
+          //hoverColor: Colors.green,
+          icon: Icon(Icons.edit),
+          onPressed: () {
+            setState(() => {
+                  editOne = true,
+                });
+          },
+        ),
         const SizedBox(height: 10),
         const SizedBox(height: 10),
       ]));
@@ -192,31 +245,45 @@ class MyMonthState extends State<Month> {
         Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
           const Text("Amount Spent"),
           Flexible(
-              child: TextField(
-            key: Key("spent2"),
-            textAlign: TextAlign.right,
-            controller: spentTwoController,
-            decoration: const InputDecoration(
-              hintText: 'R 0',
-            ),
-            keyboardType: TextInputType.number,
-            textInputAction: TextInputAction.done,
-            onSubmitted: (value) {
-              setState(() {
-                spent2 = spentTwoController.text;
-                totRem -= double.parse(spent2);
-                double left = double.parse(spent2);
-                totSpent += double.parse(spent2);
-                left = bud - left;
-                rem2 = left.toString();
-              });
-            },
-          ))
+              child: !editTwo
+                  ? Text('R' + spent2)
+                  : TextField(
+                      key: Key("spent2"),
+                      textAlign: TextAlign.right,
+                      controller: spentTwoController,
+                      decoration: const InputDecoration(
+                        hintText: 'R 0',
+                      ),
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.done,
+                      onSubmitted: (value) {
+                        setState(() {
+                          spent2 = spentTwoController.text;
+                          totRem -= double.parse(spent2);
+                          double left = double.parse(spent2);
+                          totSpent += double.parse(spent2);
+                          left = bud - left;
+                          rem2 = left.toString();
+                          editTwo = false;
+                        });
+                      },
+                    ))
         ]),
         const SizedBox(height: 10),
         Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [const Text("Amount Remaining"), Text("R " + rem2)]),
+        IconButton(
+          alignment: Alignment.bottomRight,
+          //color: Colors.green,
+          //hoverColor: Colors.green,
+          icon: Icon(Icons.edit),
+          onPressed: () {
+            setState(() => {
+                  editTwo = true,
+                });
+          },
+        ),
         const SizedBox(height: 10),
       ]));
 
@@ -251,31 +318,45 @@ class MyMonthState extends State<Month> {
         Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
           const Text("Amount Spent"),
           Flexible(
-              child: TextField(
-            key: Key("spent3"),
-            textAlign: TextAlign.right,
-            controller: spentThreeController,
-            decoration: const InputDecoration(
-              hintText: 'R 0',
-            ),
-            keyboardType: TextInputType.number,
-            textInputAction: TextInputAction.done,
-            onSubmitted: (value) {
-              setState(() {
-                spent3 = spentThreeController.text;
-                totRem -= double.parse(spent3);
-                double left = double.parse(spent3);
-                totSpent += double.parse(spent3);
-                left = bud - left;
-                rem3 = left.toString();
-              });
-            },
-          ))
+              child: !editThree
+                  ? Text('R' + spent3)
+                  : TextField(
+                      key: Key("spent3"),
+                      textAlign: TextAlign.right,
+                      controller: spentThreeController,
+                      decoration: const InputDecoration(
+                        hintText: 'R 0',
+                      ),
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.done,
+                      onSubmitted: (value) {
+                        setState(() {
+                          spent3 = spentThreeController.text;
+                          totRem -= double.parse(spent3);
+                          double left = double.parse(spent3);
+                          totSpent += double.parse(spent3);
+                          left = bud - left;
+                          rem3 = left.toString();
+                          editThree = false;
+                        });
+                      },
+                    ))
         ]),
         const SizedBox(height: 10),
         Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [const Text("Amount Remaining"), Text("R " + rem3)]),
+        IconButton(
+          alignment: Alignment.bottomRight,
+          //color: Colors.green,
+          //hoverColor: Colors.green,
+          icon: Icon(Icons.edit),
+          onPressed: () {
+            setState(() => {
+                  editThree = true,
+                });
+          },
+        ),
         const SizedBox(height: 10),
       ]));
 
@@ -310,31 +391,45 @@ class MyMonthState extends State<Month> {
         Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
           const Text("Amount Spent"),
           Flexible(
-              child: TextField(
-            key: Key("spent4"),
-            textAlign: TextAlign.right,
-            controller: spentFourController,
-            decoration: const InputDecoration(
-              hintText: 'R 0',
-            ),
-            keyboardType: TextInputType.number,
-            textInputAction: TextInputAction.done,
-            onSubmitted: (value) {
-              setState(() {
-                spent4 = spentFourController.text;
-                totRem -= double.parse(spent4);
-                double left = double.parse(spent4);
-                totSpent += double.parse(spent4);
-                left = bud - left;
-                rem4 = left.toString();
-              });
-            },
-          ))
+              child: !editFour
+                  ? Text('R' + spent4)
+                  : TextField(
+                      key: Key("spent4"),
+                      textAlign: TextAlign.right,
+                      controller: spentFourController,
+                      decoration: const InputDecoration(
+                        hintText: 'R 0',
+                      ),
+                      keyboardType: TextInputType.number,
+                      textInputAction: TextInputAction.done,
+                      onSubmitted: (value) {
+                        setState(() {
+                          spent4 = spentFourController.text;
+                          totRem -= double.parse(spent4);
+                          double left = double.parse(spent4);
+                          totSpent += double.parse(spent4);
+                          left = bud - left;
+                          rem4 = left.toString();
+                          editFour = false;
+                        });
+                      },
+                    ))
         ]),
         const SizedBox(height: 10),
         Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [const Text("Amount Remaining"), Text(rem4)]),
+        IconButton(
+          alignment: Alignment.bottomRight,
+          //color: Colors.green,
+          //hoverColor: Colors.green,
+          icon: Icon(Icons.edit),
+          onPressed: () {
+            setState(() => {
+                  editFour = true,
+                });
+          },
+        ),
         const SizedBox(height: 10),
       ]));
 
@@ -386,4 +481,55 @@ class MyMonthState extends State<Month> {
               child: Text("Total Amount Remaining:  " + totRem.toString())),
         ),
       ]));
+
+  Widget EstTotal() => ElevatedButton(
+        onPressed: () {
+          String message = "";
+          double comp = 0;
+          comp += widget.price;
+          compMessage = " Your estimated total is R " + comp.toString() + ". ";
+          if (comp < totBudget) {
+            message = "Your Grocery List is within budget. ";
+            comp = totBudget - comp;
+            message += "You will have R " +
+                comp.toString() +
+                " remaining after shopping.";
+          } else {
+            comp = comp - totBudget;
+            message = "You are " + comp.toString() + " over budget.";
+          }
+          setState(() {
+            compMessage += message;
+          });
+          showAlertDialog(context);
+        },
+        child: Text("compare"),
+      );
+
+  showAlertDialog(BuildContext context) {
+    // set up the button
+    Widget okButton = TextButton(
+      child: Text("OK"),
+      onPressed: () {
+        Navigator.of(context).pop();
+      },
+    );
+
+    // set up the AlertDialog
+    AlertDialog alert = AlertDialog(
+      title: Text("Compare Grocery List to Budget"),
+      content: Text(compMessage),
+      actions: [
+        okButton,
+      ],
+    );
+
+    // show the dialog
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return alert;
+      },
+    );
+  }
 }
