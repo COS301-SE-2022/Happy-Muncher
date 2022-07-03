aa3cd90fcf3e362c1c594df24cb397d9942bf684
Kajal022 <u20554240@tuks.co.za>
2022-07-04 00:05:53 +0200

    change email in profile page

---
+++ happy_mucher_frontend/lib/pages/changemail.dart
@@ -0,0 +1,100 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+import 'package:happy_mucher_frontend/pages/loginpage.dart';
+
+class ChangeEmail extends StatefulWidget {
+  const ChangeEmail({Key? key}) : super(key: key);
+
+  @override
+  _ChangeEmailState createState() => _ChangeEmailState();
+}
+
+class _ChangeEmailState extends State<ChangeEmail> {
+  final _formKey = GlobalKey<FormState>();
+
+  var newEmail = "";
+  // Create a text controller and use it to retrieve the current value
+  // of the TextField.
+
+  final newEmailController = TextEditingController();
+
+  @override
+  void dispose() {
+    // Clean up the controller when the widget is disposed.
+    newEmailController.dispose();
+    super.dispose();
+  }
+
+  final currentUser = FirebaseAuth.instance.currentUser;
+  changeEmail() async {
+    try {
+      await currentUser!.updateEmail(newEmail);
+      FirebaseAuth.instance.signOut();
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => LoginScreen()),
+      );
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.grey,
+          content: Text(
+            'Your Email has been Changed. Login again!',
+            style: TextStyle(fontSize: 18.0),
+          ),
+        ),
+      );
+    } catch (e) {}
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Change Email'),
+      ),
+      key: _formKey,
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 20, horizontal: 30),
+        child: ListView(
+          children: [
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 10.0),
+              child: TextFormField(
+                autofocus: false,
+                obscureText: false,
+                decoration: InputDecoration(
+                  labelText: 'New Email: ',
+                  hintText: 'Enter New Email',
+                  labelStyle: TextStyle(fontSize: 20.0),
+                  border: OutlineInputBorder(),
+                  errorStyle: TextStyle(color: Colors.redAccent, fontSize: 15),
+                ),
+                controller: newEmailController,
+                validator: (value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please Enter Email';
+                  }
+                  return null;
+                },
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Validate returns true if the form is valid, otherwise false.
+                /*if (_formKey.currentState.validate()) {
+                  setState(() {});
+                }*/
+                newEmail = newEmailController.text;
+                changeEmail();
+              },
+              child: Text(
+                'Change Email',
+                style: TextStyle(fontSize: 18.0),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
