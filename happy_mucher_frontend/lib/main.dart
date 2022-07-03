0442a1649e8ac820cf288a58dbeb3685f19b2ba8
Kajal022 <u20554240@tuks.co.za>
2022-07-04 00:04:49 +0200

    added routeName to main.dart

--- happy_mucher_frontend/lib/main.dart
+++ happy_mucher_frontend/lib/main.dart
@@ -5,6 +5,8 @@ import 'package:provider/provider.dart';
 
 import 'pages/loginpage.dart';
 import 'pages/signuppage.dart';
+import 'pages/profile.dart';
+
 import 'models/authentication.dart';
 import 'package:happy_mucher_frontend/pages/homepage.dart';
 
@@ -43,7 +45,8 @@ class MyApp extends StatelessWidget {
         routes: {
           SignupScreen.routeName: (ctx) => SignupScreen(),
           LoginScreen.routeName: (ctx) => LoginScreen(),
-          MyHomePage.routeName: (ctx) => MyHomePage()
+          MyHomePage.routeName: (ctx) => MyHomePage(),
+          Profile.routeName: (ctx) => Profile()
         },
       ),
     );
