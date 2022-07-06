e5446aa09d5664bdd98a5a31e5fadfb5107230d3
juwiCarrim <u20435780@tuks.co.za>
2022-07-06 19:30:25 +0200

    bug fix

--- happy_mucher_frontend/lib/models/recipe.dart
+++ happy_mucher_frontend/lib/models/recipe.dart
@@ -1,4 +1,3 @@
-
 class Recipe {
   final String name;
   final String images;
@@ -8,23 +7,26 @@ class Recipe {
       {this.name = "", this.images = "", this.rating = 0, this.totalTime = ""});
 
   factory Recipe.fromJson(dynamic data) {
-    if (data['name'] != null &&
-        data['images'][0]["hostedLargeUrl"] != null &&
-        data['rating'] != null &&
-        data['totalTime'] != null) {
-      return Recipe(
-          name: data["name"] as String,
-          images: data['images'][0]["hostedLargeUrl"] as String,
-          rating: data['rating'] as double,
-          totalTime: data['totalTime'] as String);
-    } else {
-      print("fetching nulls here");
-      return Recipe(
-          name: "" as String,
-          images: "" as String,
-          rating: 0.0 as double,
-          totalTime: 'totaltime' as String);
+    try {
+      if (data['name'] != null &&
+          data['images'][0]["hostedLargeUrl"] != null &&
+          data['rating'] != null &&
+          data['totalTime'] != null) {
+        return Recipe(
+            name: data["name"] as String,
+            images: data['images'][0]["hostedLargeUrl"] as String,
+            rating: data['rating'] as double,
+            totalTime: data['totalTime'] as String);
+      } else {}
+    } catch (e) {
+      print("Error");
     }
+    print("fetching nulls here");
+    return Recipe(
+        name: "" as String,
+        images: "" as String,
+        rating: 0.0 as double,
+        totalTime: 'totaltime' as String);
   }
   static List<Recipe> snapshotRecipes(List snapshot) {
     //return snapshot.map((dat) => Recipe.fromJson(dat)).toList();
