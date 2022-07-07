4088def781d01fb955c6eda38f1904cb9ada648c
NYZCoder <u19001836@tuks.co.za>
2022-07-07 21:44:17 +0200

    Dark mode functionality

--- happy_mucher_frontend/lib/main.dart
+++ happy_mucher_frontend/lib/main.dart
@@ -1,6 +1,9 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart' as pPrefix;
 import 'package:flutter/material.dart';
+import 'package:flutter_settings_screens/flutter_settings_screens.dart'
+    as mPrefix;
 import 'package:get_it/get_it.dart';
+import 'package:happy_mucher_frontend/pages/settings_page.dart';
 import 'package:provider/provider.dart';
 
 import 'pages/loginpage.dart';
@@ -17,16 +20,17 @@ import 'package:happy_mucher_frontend/provider/google_sign_in.dart';
 
 Future main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await mPrefix.Settings.init(cacheProvider: mPrefix.SharePreferenceCache());
   await Firebase.initializeApp();
 
-  final firestore = FirebaseFirestore.instance;
+  final firestore = pPrefix.FirebaseFirestore.instance;
   final firebaseAuth = FirebaseAuth.instance;
-
 
   GetIt.I.registerSingleton(firestore);
   GetIt.I.registerSingleton(firebaseAuth);
 
   runApp(MyApp());
+  MyMain();
 }
 
 class MyApp extends StatelessWidget {
@@ -48,8 +52,40 @@ class MyApp extends StatelessWidget {
           SignupScreen.routeName: (ctx) => SignupScreen(),
           LoginScreen.routeName: (ctx) => LoginScreen(),
           MyHomePage.routeName: (ctx) => MyHomePage(),
-          Profile.routeName: (ctx) => Profile()
+          Profile.routeName: (ctx) => Profile(),
         },
+      ),
+    );
+  }
+}
+
+class MyMain extends StatefulWidget {
+  static final String title = 'Settings';
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyMain> {
+  @override
+  Widget build(BuildContext context) {
+    return mPrefix.ValueChangeObserver<bool>(
+      cacheKey: SettingsPage.keyDarkMode,
+      defaultValue: true,
+      builder: (_, isDarkMode, __) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: MyMain.title,
+        theme: isDarkMode
+            ? ThemeData.dark().copyWith(
+                primaryColor: Colors.teal,
+                scaffoldBackgroundColor: Color(0xFF170635),
+                canvasColor: Color(0xFF170635),
+                colorScheme:
+                    ColorScheme.fromSwatch().copyWith(secondary: Colors.white),
+              )
+            : ThemeData.light().copyWith(
+                colorScheme:
+                    ColorScheme.fromSwatch().copyWith(secondary: Colors.black)),
+        home: SettingsPage(),
       ),
     );
   }
