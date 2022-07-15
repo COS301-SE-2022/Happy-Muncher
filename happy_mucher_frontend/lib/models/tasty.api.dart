a23045b5e65cad62dbb5c784fa00a66c610cbfd6
juwiCarrim <u20435780@tuks.co.za>
2022-07-15 16:13:26 +0200

    tasty api

---
+++ happy_mucher_frontend/lib/models/tasty.api.dart
@@ -0,0 +1,35 @@
+
+import 'dart:convert';
+import 'package:happy_mucher_frontend/models/tastyRecipe.dart';
+import 'package:http/http.dart' as http;
+
+class RecipeAPI {
+  static Future<List<tastyRecipe>> getTastyApi() async {
+    //API keys:
+    //u20435780: a3e79709c3mshd3559dbe7ea46a2p11b5b5jsnd4fd34ff55d0
+    var uri = Uri.https(
+        'tasty.p.rapidapi.com', '/recipes/list', {"from": "0", "size": "5"});
+    final resp = await http.get(uri, headers: {
+      "X-RapidAPI-Key": "a3e79709c3mshd3559dbe7ea46a2p11b5b5jsnd4fd34ff55d0",
+      "X-RapidAPI-Host": "tasty.p.rapidapi.com",
+      "useQueryString": 'true'
+    });
+
+    Map data = jsonDecode(resp.body);
+    List temp = [];
+    //List cals = [];
+
+    for (var i in data['results']) {
+      if (i.length == 50) {
+        temp.add(i);
+      } else if (i.length == 28) {
+        for (var j in i['recipes']) temp.add(j);
+      }
+
+      //if (i['type'] == "single recipe") cals.add(i['content']['nutrition']);
+    }
+    //print(temp[0]);
+
+    return tastyRecipe.snapshotRecipes(temp);
+  }
+}
