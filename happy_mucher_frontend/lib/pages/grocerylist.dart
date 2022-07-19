f5a38b215bb4bfd550182195923937e5c70f1f4c
Dylan-Ku <ku.dylan@gmail.com>
2022-07-19 16:30:28 +0200

    moved scanner to grocerylist

--- happy_mucher_frontend/lib/pages/grocerylist.dart
+++ happy_mucher_frontend/lib/pages/grocerylist.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:get_it/get_it.dart';
+import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:happy_mucher_frontend/dialogs/add_grocery.dialog.dart';
 import 'package:happy_mucher_frontend/dialogs/update_grocery.dialog.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
 
 class GroceryListPage extends StatefulWidget {
   const GroceryListPage({Key? key}) : super(key: key);
@@ -18,6 +22,7 @@ class GroceryListPageState extends State<GroceryListPage> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _quantityController = TextEditingController();
   final TextEditingController _expController = TextEditingController();
+  final ImagePicker _picker = ImagePicker();
 
   final FirebaseFirestore firestore = GetIt.I.get();
 
@@ -28,96 +33,167 @@ class GroceryListPageState extends State<GroceryListPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Grocery List'),
-          centerTitle: true,
-        ),
-        body: StreamBuilder(
-          stream: _products.snapshots(),
-          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
-            if (streamSnapshot.hasData) {
-              return ListView.builder(
-                key: const Key('Grocery_ListView'),
-                itemCount: streamSnapshot.data!.docs.length,
-                itemBuilder: (context, index) {
-                  final DocumentSnapshot documentSnapshot =
-                      streamSnapshot.data!.docs[index];
-                  return Slidable(
-                    key: Key(documentSnapshot['name']),
-                    startActionPane: ActionPane(
-                      motion: const ScrollMotion(),
-                      children: [
-                        SlidableAction(
-                          onPressed: (context) {
-                            setState(() {
-                              _products.doc(documentSnapshot.id).delete();
-                              ScaffoldMessenger.of(context).showSnackBar(
-                                  const SnackBar(
-                                      content: Text(
-                                          'You have successfully deleted a grocery list item')));
-                            });
-                          },
-                          backgroundColor: Colors.red,
-                          foregroundColor: Colors.white,
-                          icon: Icons.delete,
-                          label: 'Delete',
-                        ),
-                        SlidableAction(
-                          onPressed: (context) {
-                            showUpdateDialogGroceryList(
-                                context, documentSnapshot);
-                          },
-                          backgroundColor: Colors.blue,
-                          foregroundColor: Colors.white,
-                          icon: Icons.edit,
-                          label: 'Edit',
-                        )
-                      ],
-                    ),
-                    child: CheckboxListTile(
-                        controlAffinity: ListTileControlAffinity.leading,
-                        title: Text(documentSnapshot['name']),
-                        value: documentSnapshot['bought'],
-                        subtitle:
-                            Text('R' + documentSnapshot['price'].toString()),
-                        onChanged: (newValue) {
-                          _products
-                              .doc(documentSnapshot.id)
-                              .update({'bought': !documentSnapshot['bought']});
-
-                          var checkVal = documentSnapshot['bought'];
-                          var itemName = documentSnapshot['name'];
-                          if (checkVal == false) {
-                            //checks previous value if it is changing from false to true that means its being bought
-                            _inventory.add(
-                              {
-                                "itemName": itemName,
-                                "quantity": 1,
-                                "expirationDate": ""
-                              },
-                            );
+      appBar: AppBar(
+        title: const Text('Grocery List'),
+        centerTitle: true,
+      ),
+      body: StreamBuilder(
+        stream: _products.snapshots(),
+        builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+          if (streamSnapshot.hasData) {
+            return ListView.builder(
+              key: const Key('Grocery_ListView'),
+              itemCount: streamSnapshot.data!.docs.length,
+              itemBuilder: (context, index) {
+                final DocumentSnapshot documentSnapshot =
+                    streamSnapshot.data!.docs[index];
+                return Slidable(
+                  key: Key(documentSnapshot['name']),
+                  startActionPane: ActionPane(
+                    motion: const ScrollMotion(),
+                    children: [
+                      SlidableAction(
+                        onPressed: (context) {
+                          setState(() {
+                            _products.doc(documentSnapshot.id).delete();
                             ScaffoldMessenger.of(context).showSnackBar(
                                 const SnackBar(
                                     content: Text(
-                                        'Please go to the inventory page to edit the quantity and expiration date')));
-                          }
-                        }),
-                  );
-                },
-              );
-            }
+                                        'You have successfully deleted a grocery list item')));
+                          });
+                        },
+                        backgroundColor: Colors.red,
+                        foregroundColor: Colors.white,
+                        icon: Icons.delete,
+                        label: 'Delete',
+                      ),
+                      SlidableAction(
+                        onPressed: (context) {
+                          showUpdateDialogGroceryList(
+                              context, documentSnapshot);
+                        },
+                        backgroundColor: Colors.blue,
+                        foregroundColor: Colors.white,
+                        icon: Icons.edit,
+                        label: 'Edit',
+                      )
+                    ],
+                  ),
+                  child: CheckboxListTile(
+                      controlAffinity: ListTileControlAffinity.leading,
+                      title: Text(documentSnapshot['name']),
+                      value: documentSnapshot['bought'],
+                      subtitle:
+                          Text('R' + documentSnapshot['price'].toString()),
+                      onChanged: (newValue) {
+                        _products
+                            .doc(documentSnapshot.id)
+                            .update({'bought': !documentSnapshot['bought']});
 
-            return const Center(
-              child: CircularProgressIndicator(),
+                        var checkVal = documentSnapshot['bought'];
+                        var itemName = documentSnapshot['name'];
+                        if (checkVal == false) {
+                          //checks previous value if it is changing from false to true that means its being bought
+                          _inventory.add(
+                            {
+                              "itemName": itemName,
+                              "quantity": 1,
+                              "expirationDate": ""
+                            },
+                          );
+                          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                              content: Text(
+                                  'Please go to the inventory page to edit the quantity and expiration date')));
+                        }
+                      }),
+                );
+              },
             );
-          },
-        ),
-        floatingActionButton: FloatingActionButton(
-          key: const Key('addToGroceryListButton'),
-          onPressed: () => addGLDialog(context),
-          child: const Icon(Icons.add),
-        ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat);
+          }
+
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      ),
+      floatingActionButton: SpeedDial(
+        icon: Icons.add,
+        children: [
+          SpeedDialChild(
+            onTap: () => addGLDialog(context),
+            key: const Key('addToInventoryButton'),
+            child: const Icon(
+              Icons.abc,
+              color: Colors.white,
+            ),
+            backgroundColor: Colors.blue,
+          ),
+          SpeedDialChild(
+            onTap: () async {
+              captureImageReceipt(ImageSource.gallery);
+            },
+            child: const Icon(
+              Icons.collections,
+              color: Colors.white,
+            ),
+            backgroundColor: Colors.blue,
+          ),
+          SpeedDialChild(
+            onTap: () async {
+              captureImageReceipt(ImageSource.camera);
+            },
+            child: const Icon(
+              Icons.photo_camera,
+              color: Colors.white,
+            ),
+            backgroundColor: Colors.blue,
+          )
+        ],
+      ),
+    );
+    // floatingActionButton: FloatingActionButton(
+    //   key: const Key('addToGroceryListButton'),
+    //   onPressed: () => addGLDialog(context),
+    //   child: const Icon(Icons.add),
+    // ),
+    // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat);
+  }
+
+  void captureImageReceipt(ImageSource imageSource) async {
+    final image = await _picker.pickImage(source: imageSource);
+    if (image == null) {
+      return;
+    }
+    final croppedImagePath = await cropImage(image.path);
+    if (croppedImagePath == null) {
+      return;
+    }
+    getRecognisedText(croppedImagePath);
+  }
+
+  Future<String?> cropImage(String path) async {
+    final cropped =
+        await ImageCropper().cropImage(sourcePath: path, uiSettings: [
+      AndroidUiSettings(
+          toolbarTitle: 'Croppper',
+          initAspectRatio: CropAspectRatioPreset.original,
+          lockAspectRatio: false)
+    ]);
+    return cropped?.path;
+  }
+
+  void getRecognisedText(String path) async {
+    final inputImage = InputImage.fromFilePath(path);
+    final textDetector = GoogleMlKit.vision.textRecognizer();
+    RecognizedText recognizedText = await textDetector.processImage(inputImage);
+    await textDetector.close();
+    var scanText = "";
+    for (TextBlock block in recognizedText.blocks) {
+      for (TextLine line in block.lines) {
+        scanText = scanText + line.text + "\n";
+      }
+    }
+    print(scanText);
   }
 }
 
