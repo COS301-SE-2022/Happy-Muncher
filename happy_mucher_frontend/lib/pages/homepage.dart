5ee4de6a76836b321188f3bf384dce7a64562f1f
NYZCoder <u19001836@tuks.co.za>
2022-07-19 15:56:00 +0200

    Adding cards to homepage
    Co-authored-by: juwiCarrim <juwiCarrim@users.noreply.github.com>

--- happy_mucher_frontend/lib/pages/homepage.dart
+++ happy_mucher_frontend/lib/pages/homepage.dart
@@ -13,10 +13,7 @@ import 'package:happy_mucher_frontend/pages/changepassword.dart';
 import 'package:happy_mucher_frontend/pages/navbar.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart';
 
-
-
 import 'package:happy_mucher_frontend/pages/recipebook.dart';
-
 
 void main() {
   runApp(MyApp());
@@ -64,117 +61,166 @@ class _MyHomePageState extends State<MyHomePage> {
         ],
       ),
       drawer: NavBar(),
-      body: Center(
+      body: SingleChildScrollView(
         child: Column(
           mainAxisSize: MainAxisSize.min,
-          children: [
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: RaisedButton(
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 30),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => GroceryListPage()));
-                },
-                color: Colors.blue,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30))),
-                child: Text(
-                  "View Grocery List",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: RaisedButton(
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 30),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => IventoryPage()));
-                },
-                color: Colors.blue,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30))),
-                child: Text(
-                  "View Inventory",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: RaisedButton(
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 30),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Budget()));
-                },
-                color: Colors.blue,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30))),
-                child: Text(
-                  "Create Your Budget",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: RaisedButton(
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 30),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => MealPage()));
-                },
-                color: Colors.blue,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30))),
-                child: Text(
-                  "Plan a meal",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
-            SizedBox(
-              height: 16,
-            ),
-            SizedBox(
-              width: 200,
-              height: 50,
-              child: RaisedButton(
-                padding: EdgeInsets.symmetric(vertical: 8, horizontal: 30),
-                onPressed: () {
-                  Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => RecipeBook()));
-                },
-                color: Colors.blue,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(30))),
-                child: Text(
-                  "Recipe Book",
-                  style: TextStyle(color: Colors.white),
-                ),
-              ),
-            ),
+          children: <Widget>[
+            const SizedBox(height: 10),
+            buildGrocerycard(),
+            const SizedBox(height: 10),
+            buildInventorycard(),
+            const SizedBox(height: 10),
+            buildBudgetcard(),
+            const SizedBox(height: 10),
+            buildMealcard(),
+            const SizedBox(height: 10),
+            buildRecipecard(),
           ],
         ),
       ),
     );
   }
+
+  Widget buildGrocerycard() => Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Ink.image(
+              image: AssetImage('assets/images/budget.jpg'),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => GroceryListPage()));
+                },
+              ),
+              height: 180,
+              fit: BoxFit.cover,
+            ),
+            Text(
+              'Grocery List',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            )
+          ],
+        ),
+      );
+  Widget buildInventorycard() => Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Ink.image(
+              image: AssetImage('assets/images/budget.jpg'),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => IventoryPage()));
+                },
+              ),
+              height: 180,
+              fit: BoxFit.cover,
+            ),
+            Text(
+              'View Inventory',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            )
+          ],
+        ),
+      );
+  Widget buildBudgetcard() => Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Ink.image(
+              image: AssetImage('assets/images/budget.jpg'),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => Budget()));
+                },
+              ),
+              height: 180,
+              fit: BoxFit.cover,
+            ),
+            Text(
+              'Budget Planner',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            )
+          ],
+        ),
+      );
+  Widget buildMealcard() => Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Ink.image(
+              image: AssetImage('assets/images/budget.jpg'),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MealPage()));
+                },
+              ),
+              height: 180,
+              fit: BoxFit.cover,
+            ),
+            Text(
+              'Meal Planner',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            )
+          ],
+        ),
+      );
+  Widget buildRecipecard() => Card(
+        clipBehavior: Clip.antiAlias,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(24)),
+        child: Stack(
+          alignment: Alignment.center,
+          children: [
+            Ink.image(
+              image: AssetImage('assets/images/budget.jpg'),
+              child: InkWell(
+                onTap: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => RecipeBook()));
+                },
+              ),
+              height: 180,
+              fit: BoxFit.cover,
+            ),
+            Text(
+              'Recipe Book',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+                fontSize: 24,
+              ),
+            )
+          ],
+        ),
+      );
 }
