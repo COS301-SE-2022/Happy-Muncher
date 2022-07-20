1706dbfe34b3e39416b314a3d47cc88fdd4b5545
Kajal022 <u20554240@tuks.co.za>
2022-07-21 00:27:16 +0200

    changed colours in styles.dart

--- happy_mucher_frontend/lib/models/styles.dart
+++ happy_mucher_frontend/lib/models/styles.dart
@@ -83,7 +83,7 @@ abstract class Styles {
       indicatorColor: isDarkTheme ? Color(0xff0E1D36) : Color(0xffCBDCF8),
       //buttonColor: isDarkTheme ? Color(0xff3B3B3B) : Color(0xffF1F5FB),
 
-      hintColor: isDarkTheme ? Color(0xff280C0B) : Color(0xffEECED3),
+      hintColor: isDarkTheme ? Colors.grey : Color(0xffEECED3),
 
       highlightColor: isDarkTheme ? Color(0xff372901) : Color(0xffFCE192),
       hoverColor: isDarkTheme ? Color(0xff3A3A3B) : Color(0xff4285F4),
