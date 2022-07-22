b9fa6231f030352c78e0cd8c34bbace314865c24
Dylan-Ku <ku.dylan@gmail.com>
2022-07-23 00:17:50 +0200

    removed unnecessary code

--- happy_mucher_frontend/lib/pages/inventory.dart
+++ happy_mucher_frontend/lib/pages/inventory.dart
@@ -1,13 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:get_it/get_it.dart';
-import 'package:google_ml_kit/google_ml_kit.dart';
 import 'package:happy_mucher_frontend/dialogs/add_inventory.dialog.dart';
 import 'package:happy_mucher_frontend/dialogs/update_inventory.dialog.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:flutter_speed_dial/flutter_speed_dial.dart';
-import 'package:image_cropper/image_cropper.dart';
-import 'package:image_picker/image_picker.dart';
 
 class IventoryPage extends StatefulWidget {
   const IventoryPage({Key? key}) : super(key: key);
@@ -22,7 +18,6 @@ class _IventoryPageState extends State<IventoryPage> {
   final TextEditingController _nameController = TextEditingController();
   final TextEditingController _quantityController = TextEditingController();
   final TextEditingController _expController = TextEditingController();
-  final ImagePicker _picker = ImagePicker();
 
   final FirebaseFirestore firestore = GetIt.I.get();
 
@@ -93,85 +88,13 @@ class _IventoryPageState extends State<IventoryPage> {
         },
       ),
 // Add new product
-      // floatingActionButton: FloatingActionButton(
-      //   key: const Key('addToInventoryButton'),
-      //   onPressed: () => addInventoryDialog(context),
-      //   child: const Icon(Icons.add),
-      // ),
-      // floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-
-      floatingActionButton: SpeedDial(
-        icon: Icons.add,
-        children: [
-          SpeedDialChild(
-            onTap: () => addInventoryDialog(context),
-            key: const Key('addToInventoryButton'),
-            child: const Icon(
-              Icons.abc,
-              color: Colors.white,
-            ),
-            backgroundColor: Colors.blue,
-          ),
-          SpeedDialChild(
-            onTap: () async {
-              captureImageReceipt(ImageSource.gallery);
-            },
-            child: const Icon(
-              Icons.collections,
-              color: Colors.white,
-            ),
-            backgroundColor: Colors.blue,
-          ),
-          SpeedDialChild(
-            onTap: () async {
-              captureImageReceipt(ImageSource.camera);
-            },
-            child: const Icon(
-              Icons.photo_camera,
-              color: Colors.white,
-            ),
-            backgroundColor: Colors.blue,
-          )
-        ],
+      floatingActionButton: FloatingActionButton(
+        key: const Key('addToInventoryButton'),
+        onPressed: () => addInventoryDialog(context),
+        child: const Icon(Icons.add),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
-  }
-
-  void captureImageReceipt(ImageSource imageSource) async {
-    final image = await _picker.pickImage(source: imageSource);
-    if (image == null) {
-      return;
-    }
-    final croppedImagePath = await cropImage(image.path);
-    if (croppedImagePath == null) {
-      return;
-    }
-    getRecognisedText(croppedImagePath);
-  }
-
-  Future<String?> cropImage(String path) async {
-    final cropped =
-        await ImageCropper().cropImage(sourcePath: path, uiSettings: [
-      AndroidUiSettings(
-          toolbarTitle: 'Croppper',
-          initAspectRatio: CropAspectRatioPreset.original,
-          lockAspectRatio: false)
-    ]);
-    return cropped?.path;
-  }
-
-  void getRecognisedText(String path) async {
-    final inputImage = InputImage.fromFilePath(path);
-    final textDetector = GoogleMlKit.vision.textRecognizer();
-    RecognizedText recognizedText = await textDetector.processImage(inputImage);
-    await textDetector.close();
-    var scanText = "";
-    for (TextBlock block in recognizedText.blocks) {
-      for (TextLine line in block.lines) {
-        scanText = scanText + line.text + "\n";
-      }
-    }
-    print(scanText);
   }
 }
 
