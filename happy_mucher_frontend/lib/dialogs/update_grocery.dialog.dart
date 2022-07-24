34c1d2846c504851f2274607e7ace383c66d1281
Dylan-Ku <ku.dylan@gmail.com>
2022-07-24 14:53:49 +0200

    removed unused import

--- happy_mucher_frontend/lib/dialogs/update_grocery.dialog.dart
+++ happy_mucher_frontend/lib/dialogs/update_grocery.dialog.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:intl/intl.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<void> showUpdateDialogGroceryList(
