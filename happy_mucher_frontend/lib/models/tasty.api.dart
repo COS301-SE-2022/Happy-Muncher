0cafe26e7f14b4fae7834f11ef319a0bc6f01b57
juwiCarrim <u20435780@tuks.co.za>
2022-07-26 23:04:49 +0200

    secrets

--- happy_mucher_frontend/lib/models/tasty.api.dart
+++ happy_mucher_frontend/lib/models/tasty.api.dart
@@ -1,16 +1,20 @@
 import 'dart:convert';
 import 'package:happy_mucher_frontend/models/tastyRecipe.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class TastyRecipeAPI {
   static Future<List<tastyRecipe>> getTastyApi(String query) async {
-    //API keys:
-    //u20435780: a3e79709c3mshd3559dbe7ea46a2p11b5b5jsnd4fd34ff55d0
-    //juwi: 282369d33bmsh7ef42e05ee4457ep10316fjsna59a1676c1de
+    
+    //dotenv.env['JUWI_API_KEY'];
+    await dotenv.load(fileName: "secrets.env");
+    //await dotenv.load();
+    String key = dotenv.get('JUWI_API_KEY');
+
     var uri = Uri.https(
         'tasty.p.rapidapi.com', '/recipes/list', {"from": "0", "size": "20"});
     final resp = await http.get(uri, headers: {
-      "X-RapidAPI-Key": "a3e79709c3mshd3559dbe7ea46a2p11b5b5jsnd4fd34ff55d0",
+      "X-RapidAPI-Key": key,
       "X-RapidAPI-Host": "tasty.p.rapidapi.com",
       "useQueryString": 'true'
     });
