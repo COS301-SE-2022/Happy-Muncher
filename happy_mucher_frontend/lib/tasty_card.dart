98b5f166b6ae34a672d5e41a0da64b7acce38823
Kajal022 <u20554240@tuks.co.za>
2022-08-27 15:45:30 +0200

    connecting to user db

--- happy_mucher_frontend/lib/tasty_card.dart
+++ happy_mucher_frontend/lib/tasty_card.dart
@@ -8,8 +8,10 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter_speed_dial/flutter_speed_dial.dart';
 import 'package:happy_mucher_frontend/dialogs/add_recipe.dialog.dart';
 import 'package:happy_mucher_frontend/pages/mealplanner.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 class TastyRecipeCard extends StatelessWidget {
+  final uid = FirebaseAuth.instance.currentUser!.uid;
   final String name;
   final String images;
   final int recipeid;
@@ -30,7 +32,8 @@ class TastyRecipeCard extends StatelessWidget {
       this.instructions = const ['']});
 
   final FirebaseFirestore firestore = GetIt.I.get();
-  CollectionReference get _meals => firestore.collection('Meal Planner');
+  CollectionReference get _meals =>
+      firestore.collection('Users').doc(uid).collection('Meal Planner');
 
   @override
   Widget build(BuildContext context) {
