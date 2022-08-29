3a10a6cbcb42c36129cc7893e8755dd03a3b5a3b
Kajal Bhoola <u20554240@tuks.co.za>
2022-08-29 21:45:36 +0200

    Update update_grocery.dialog.dart

--- happy_mucher_frontend/lib/dialogs/update_grocery.dialog.dart
+++ happy_mucher_frontend/lib/dialogs/update_grocery.dialog.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:happy_mucher_frontend/pages/grocerylist.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 Future<void> showUpdateDialogGroceryList(
     BuildContext context, DocumentSnapshot document) {
@@ -24,15 +24,15 @@ class GLDialog extends StatefulWidget {
 class _UpdateGLPageState extends State<GLDialog> {
   // text fields' controllers
   // text fields' controllers
+  final uid = FirebaseAuth.instance.currentUser!.uid;
   final nameController = TextEditingController();
   final priceController = TextEditingController();
   final dateFieldController = TextEditingController();
 
   final FirebaseFirestore firestore = GetIt.I.get();
 
-  CollectionReference get _items => firestore.collection('GroceryList');
-
-  CollectionReference get _totals => firestore.collection('GL totals');
+  CollectionReference get _items =>
+      firestore.collection('Users').doc(uid).collection('GroceryList');
 
   DateTime? expirationDate;
   DocumentSnapshot documentSnapshot;
@@ -85,13 +85,10 @@ class _UpdateGLPageState extends State<GLDialog> {
             final price = priceController.text;
             final priceDouble = double.tryParse(price);
             if (priceDouble != null) {
-              double ogPrice = documentSnapshot['price'];
               await _items
                   .doc(documentSnapshot.id)
                   .update({"name": name, "price": priceDouble});
-              UpdateGL(priceDouble, ogPrice);
-              setState(() {});
-              //GroceryListPageState().getTotals();
+
               nameController.text = '';
               priceController.text = '';
               dateFieldController.text = '';
@@ -102,24 +99,5 @@ class _UpdateGLPageState extends State<GLDialog> {
         )
       ],
     );
-  }
-
-  void UpdateGL(double price, double og) async {
-    String? e = '';
-    double estimate = 0.0;
-    var collection = FirebaseFirestore.instance.collection('GL totals');
-    //userUid is the current auth user
-    var docSnapshot = await collection.doc('Totals').get();
-
-    Map<String, dynamic> data = docSnapshot.data()!;
-
-    e = data['estimated total'].toString();
-
-    estimate = double.parse(e);
-    estimate = estimate - og;
-    estimate += price;
-    await _totals.doc('Totals').update({
-      "estimated total": estimate,
-    });
   }
 }
