f35e4b141e6c37771ec23f0614cc97d801530860
Ponalo Notwane <u16115092@tuks.co.za>
2022-09-04 02:04:45 +0200

    Small detail added to the Navbar

--- happy_mucher_frontend/lib/pages/NavBar.dart
+++ happy_mucher_frontend/lib/pages/NavBar.dart
@@ -3,6 +3,22 @@ import 'package:flutter/material.dart';
 class NavBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Drawer();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bottom Nav Bar'),
+      ),
+      body: Text('Home'),
+    );
   }
 }
+
+/*class _NavBarState extends State<NavBar> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Bottom Nav Bar'),
+      ),
+      body: Text(),
+    );
+  }
+}*/
