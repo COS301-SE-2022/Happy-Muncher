7810f232678b304ea40d76565c87d5b8cb76ec35
Kajal022 <u20554240@tuks.co.za>
2022-09-05 18:15:15 +0200

    changed graph values

--- happy_mucher_frontend/lib/pages/values.dart
+++ happy_mucher_frontend/lib/pages/values.dart
@@ -1,13 +1,13 @@
 class Values {
-  final String budget;
-  final num total_remaining;
+  final double budget;
+  final String total_remaining;
   Values(this.budget, this.total_remaining);
 
   Values.fromMap(dynamic map)
       : assert(map['budget'] != null),
-        assert(map['total remaining'] != null),
+        assert(map['month'] != null),
         budget = map['budget'],
-        total_remaining = map['total remaining'];
+        total_remaining = map['month'];
 
   @override
   String toString() => "Record<$budget:$total_remaining>";
