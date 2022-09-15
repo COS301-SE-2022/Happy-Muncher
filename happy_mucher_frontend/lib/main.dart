71fd7e4efe131210c55aa16926f77fe1f399c805
Kajal022 <u20554240@tuks.co.za>
2022-09-15 09:14:37 +0200

    fixes

--- happy_mucher_frontend/lib/main.dart
+++ happy_mucher_frontend/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
-import 'package:happy_mucher_frontend/pages/dashboard.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_settings_screens/flutter_settings_screens.dart'
     as mPrefix;
