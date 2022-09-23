819c8693ee95753695c268790189df858ee9f4af
Kajal022 <u20554240@tuks.co.za>
2022-09-23 11:04:56 +0200

    updated values for bugdet card

--- happy_mucher_frontend/lib/pages/values.dart
+++ happy_mucher_frontend/lib/pages/values.dart
@@ -1,14 +1,17 @@
 class Values {
   final double budget;
   final String month;
+  final num totalSpent;
 
-  Values(this.budget, this.month);
+  Values(this.budget, this.month, this.totalSpent);
 
   Values.fromMap(dynamic map)
       : assert(map['budget'] != null),
         assert(map['month'] != null),
+        assert(map['total spent'] != null),
         budget = map['budget'],
-        month = map['month'];
+        month = map['month'],
+        totalSpent = map['total spent'];
 
   @override
   String toString() => "Record<$budget:$month>";
