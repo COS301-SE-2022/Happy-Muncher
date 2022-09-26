a1982580186d8b7fa73d64d7f9666c0c29eb57f1
Dylan-Ku <ku.dylan@gmail.com>
2022-09-26 18:11:07 +0200

    test adding ingredient

--- happy_mucher_frontend/test/user_recipe_test.dart
+++ happy_mucher_frontend/test/user_recipe_test.dart
@@ -176,6 +176,56 @@ void main() {
           await tester.enterText(textboxes, '10');
         },
       );
+
+      testWidgets(
+        'Testing adding to ingredients',
+        (WidgetTester tester) async {
+          //test if page starts properly
+          await tester.pumpWidget(testApp);
+
+          await tester.pumpAndSettle(const Duration(milliseconds: 300));
+
+          final lastTextField = find.byKey(const Key('entercalories'));
+
+          expect(lastTextField, findsOneWidget);
+
+          await tester.drag(lastTextField, const Offset(0.0, -300));
+
+          await tester.pumpAndSettle(const Duration(milliseconds: 300));
+
+          final speeddials =
+              find.byKey(const Key('speed_dial_button_ingredients'));
+
+          expect(speeddials, findsOneWidget);
+
+          await tester.tap(speeddials);
+          await tester.pumpAndSettle(const Duration(milliseconds: 300));
+
+          final textButton =
+              find.byKey(const Key('addToIngredientsButtonText'));
+
+          expect(textButton, findsOneWidget);
+
+          await tester.tap(textButton);
+          await tester.pumpAndSettle(const Duration(milliseconds: 300));
+
+          final dialogText = find.byKey(const Key('dialog_ingredient'));
+
+          expect(dialogText, findsOneWidget);
+
+          await tester.enterText(dialogText, "test ingredient");
+
+          final dialogButton = find.byKey(const Key('addButton'));
+
+          expect(dialogButton, findsOneWidget);
+
+          await tester.tap(dialogButton);
+          await tester.pumpAndSettle(const Duration(milliseconds: 300));
+
+          final ingredient = find.text("test ingredient");
+          expect(ingredient, findsOneWidget);
+        },
+      );
     },
   );
 }
